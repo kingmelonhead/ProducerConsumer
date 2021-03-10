be537c6feeec91862e427bcec7be95ff9529b290
John Hackstadt <jhackstadt50@gmail.com>
2021-03-09 21:24:27 -0600

    added logging functionality

--- lib_mon.c
+++ lib_mon.c
@@ -20,6 +20,10 @@ int sem_id, buffer_id;
 
 void append(int x){
 
+    time_t time_start, time_end;
+    struct tm * time_start_info;
+    struct tm * time_end_info;
+
 	//gets shared memory for the buffer
 	key_t buffer_key = ftok(".", 'a');
 	buffer_id = shmget(buffer_key, sizeof(int) * 4, IPC_EXCL);
@@ -29,6 +33,11 @@ void append(int x){
     FILE *file_ptr;
     file_ptr = fopen("output.log", "a");
 
+    time(&time_start);
+    time_start_info = localtime(&time_start);
+
+    fprintf(file_ptr, "Writing %d to buffer and sleeping for 1 second. Time: %s \n", x, asctime(time_start_info));
+
     //put x in buffer and log this action
     //buffer[nextin] = x
 
@@ -36,6 +45,9 @@ void append(int x){
     sleep(1);
 
     //log the time post sleep and notify that exiting
+    time(&time_end);
+    time_end_info = localtime(&time_end);
+    fprintf(file_ptr, "Writing and sleeping done, ending now. Time %s \n\n", asctime(time_end_info));
 
     //clean up 
     fclose(file_ptr);
@@ -44,7 +56,11 @@ void append(int x){
 
 void consume(){
 
-    int food;
+    time_t time_start, time_end;
+    struct tm * time_start_info;
+    struct tm * time_end_info;
+
+    int food = 0;
 
     //gets shared semaphore array
 	key_t sem_key = ftok("./README", 'a');
@@ -59,18 +75,22 @@ void consume(){
     FILE *file_ptr;
     file_ptr = fopen("output.log", "a");
 
+    time(&time_start);
+    time_start_info = localtime(&time_start);
     //consume
     //food = whatever is in buffer
     //log this number and time
-
+    fprintf(file_ptr, "Got %d from the buffer. This will be consumed after sleeping for 1 second (doubled, logged, then deleted from buffer). Time: %s", food, asctime(time_start_info));
     //sleep for 1 second
     sleep(1);
 
     //do stuff (double the number in the buffer)
-    //food *= 2 
+    food *= 2;
 
+    time(&time_end);
+    time_end_info = localtime(&time_end);
     //log the new number, and state what was done. as well as record the time
-
+    fprintf(file_ptr, "The product in the buffer, after being doubled is %d. Time: %s  ...now ending\n\n", food, asctime(time_end_info));
     //clean up 
     fclose(file_ptr);
     shmdt(buffer_ptr);
